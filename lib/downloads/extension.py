1e7802cce0c2f46188566058aa7e2db3720e7f80
Daniel Mikusa <dmikusa@pivotal.io>
2014-10-15 12:57:24 -0400

    Added downloads extension, which downloads files, extracts them into the application directory and then runs the specified build pack.  Also includes tests for checking a couple different application types.

---
+++ lib/downloads/extension.py
@@ -0,0 +1,80 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import tempfile
+from build_pack_utils import BuildPack
+
+
+def preprocess_commands(ctx):
+    return ()
+
+
+def service_commands(ctx):
+    release_path = os.path.join(tempfile.gettempdir(), 'release.out')
+    with open(release_path) as fp:
+        webFound = False
+        lines = []
+        for line in fp.readlines():
+            if line.startswith('  web:'):
+                webFound = True
+                lines.append(line.replace('  web:', '').strip())
+            elif webFound and line.startswith('    '):
+                lines.append(line.strip())
+        proc = " ".join(lines)
+    return {
+        'web': [proc]
+    }
+        
+
+def service_environment(ctx):
+    return {}
+
+
+def download(install, link):
+    installer = install._installer
+    ctx = install.builder._ctx
+    installer.install_binary_direct(
+        link,
+        'NO HASH',  # skipping hash on purpose
+        ctx['BUILD_DIR'])
+
+
+def compile(install):
+    ctx = install.builder._ctx
+    # read links file
+    with open(os.path.join(ctx['BUILD_DIR'], 'download-list.txt')) as fp:
+        links = [line.strip() for line in fp.readlines()]
+    # download links
+    for link in links:
+        if link.startswith('http'):
+            download(install, link)
+        else:
+            print 'Not sure how to handle [%s], skipping.' % link
+    # read build pack
+    with open(os.path.join(ctx['BUILD_DIR'], 'build-pack.txt')) as fp:
+        bp_link = fp.read().strip()
+    if bp_link.find('#') >= 0:
+        (bp_link, bp_ver) = bp_link.split('#')
+        bp = BuildPack(ctx, bp_link, bp_ver)
+    else:
+        bp = BuildPack(ctx, bp_link)
+    # run build pack
+    bp._clone()
+    bp._compile()
+    # save output from release
+    release_path = os.path.join(tempfile.gettempdir(), 'release.out')
+    with open(release_path, 'wt') as fp:
+        fp.write(bp._release())
+    return 0
---
+++ tests/test_compile.py
@@ -0,0 +1,177 @@
+import shutil
+import tempfile
+import os.path
+import json
+from nose.tools import eq_
+from build_pack_utils import BuildPack
+
+
+class BaseTestCompile(object):
+    def initialize(self, app):
+        self.build_dir = tempfile.mkdtemp(prefix='build-')
+        self.cache_dir = tempfile.mkdtemp(prefix='cache-')
+        os.rmdir(self.build_dir)  # delete otherwise copytree complains
+        os.rmdir(self.cache_dir)  # cache dir does not exist normally
+        shutil.copytree('tests/data/%s' % app, self.build_dir)
+
+    def copy_build_pack(self, bp_dir):
+        # simulate clone, makes debugging easier
+        os.rmdir(bp_dir)
+        shutil.copytree('.', bp_dir,
+                        ignore=shutil.ignore_patterns(".git",
+                                                      "binaries",
+                                                      "env",
+                                                      "tests"))
+
+    def cleanup(self):
+        if os.path.exists(self.build_dir):
+            shutil.rmtree(self.build_dir)
+        if os.path.exists(self.cache_dir):
+            shutil.rmtree(self.cache_dir)
+        for name in os.listdir(os.environ['TMPDIR']):
+            if name.startswith('httpd-') and name.endswith('.gz'):
+                os.remove(os.path.join(os.environ['TMPDIR'], name))
+            if name.startswith('nginx-') and name.endswith('.gz'):
+                os.remove(os.path.join(os.environ['TMPDIR'], name))
+            if name.startswith('php-') and name.endswith('.gz'):
+                os.remove(os.path.join(os.environ['TMPDIR'], name))
+
+    def assert_exists(self, *args):
+        eq_(True, os.path.exists(os.path.join(*args)),
+            "Does not exists: %s" % os.path.join(*args))
+
+
+class TestApp1(BaseTestCompile):
+    def setUp(self):
+        BaseTestCompile.initialize(self, 'app-1')
+
+    def tearDown(self):
+        BaseTestCompile.cleanup(self)
+
+    def test_setup(self):
+        eq_(True, os.path.exists(self.build_dir))
+        eq_(False, os.path.exists(self.cache_dir))
+
+    def test_compile(self):
+        bp = BuildPack({
+            'BUILD_DIR': self.build_dir,
+            'CACHE_DIR': self.cache_dir
+        }, '.')
+        self.copy_build_pack(bp.bp_dir)
+        try:
+            output = ''
+            output = bp._compile()
+            self.assert_exists(self.build_dir, 'snake')
+            self.assert_exists(self.build_dir, 'snake', 'snake.html')
+            self.assert_exists(self.build_dir, '.bp')
+            self.assert_exists(self.build_dir, '.env')
+            self.assert_exists(self.build_dir, '.procs')
+            self.assert_exists(self.build_dir, 'start.sh')
+            # check release command
+            with open(os.path.join(self.build_dir, '.procs')) as fp:
+                procs = [l.strip() for l in fp.readlines()]
+            eq_(1, len(procs))
+            eq_(True, procs[0].startswith('web:'))
+        except Exception, e:
+            print str(e)
+            if hasattr(e, 'output'):
+                print e.output
+            if output:
+                print output
+            raise
+        finally:
+            if os.path.exists(bp.bp_dir):
+                shutil.rmtree(bp.bp_dir)
+
+class TestApp2(BaseTestCompile):
+    def setUp(self):
+        BaseTestCompile.initialize(self, 'app-2')
+
+    def tearDown(self):
+        BaseTestCompile.cleanup(self)
+
+    def test_setup(self):
+        eq_(True, os.path.exists(self.build_dir))
+        eq_(False, os.path.exists(self.cache_dir))
+
+    def test_compile(self):
+        bp = BuildPack({
+            'BUILD_DIR': self.build_dir,
+            'CACHE_DIR': self.cache_dir
+        }, '.')
+        self.copy_build_pack(bp.bp_dir)
+        try:
+            output = ''
+            output = bp._compile()
+            self.assert_exists(self.build_dir, 'META-INF')
+            self.assert_exists(self.build_dir, 'WEB-INF')
+            self.assert_exists(self.build_dir, '.bp')
+            self.assert_exists(self.build_dir, '.env')
+            self.assert_exists(self.build_dir, '.procs')
+            self.assert_exists(self.build_dir, '.java-buildpack')
+            self.assert_exists(self.build_dir, '.java-buildpack.log')
+            self.assert_exists(self.build_dir, 'start.sh')
+            # check release command
+            with open(os.path.join(self.build_dir, '.procs')) as fp:
+                procs = [l.strip() for l in fp.readlines()]
+            eq_(1, len(procs))
+            eq_(True, procs[0].startswith('web:'))
+            eq_(True, procs[0].find('http.port=$PORT') >= 0)
+            eq_(True, procs[0].find('MetaspaceSize=64M') >= 0)
+            eq_(True, procs[0].find('OnOutOfMemoryError') >= 0)
+        except Exception, e:
+            print str(e)
+            if hasattr(e, 'output'):
+                print e.output
+            if output:
+                print output
+            raise
+        finally:
+            if os.path.exists(bp.bp_dir):
+                shutil.rmtree(bp.bp_dir)
+
+class TestApp3(BaseTestCompile):
+    def setUp(self):
+        BaseTestCompile.initialize(self, 'app-3')
+
+    def tearDown(self):
+        BaseTestCompile.cleanup(self)
+
+    def test_setup(self):
+        eq_(True, os.path.exists(self.build_dir))
+        eq_(False, os.path.exists(self.cache_dir))
+
+    def test_compile(self):
+        bp = BuildPack({
+            'BUILD_DIR': self.build_dir,
+            'CACHE_DIR': self.cache_dir
+        }, '.')
+        self.copy_build_pack(bp.bp_dir)
+        try:
+            output = ''
+            output = bp._compile()
+            self.assert_exists(self.build_dir, 'httpd')
+            self.assert_exists(self.build_dir, 'php')
+            self.assert_exists(self.build_dir, 'htdocs')
+            self.assert_exists(self.build_dir, '.bp')
+            self.assert_exists(self.build_dir, '.env')
+            self.assert_exists(self.build_dir, '.procs')
+            self.assert_exists(self.build_dir, 'start.sh')
+            # check release command
+            with open(os.path.join(self.build_dir, '.procs')) as fp:
+                procs = [l.strip() for l in fp.readlines()]
+            eq_(4, len(procs))
+            eq_(True, procs[0].startswith('httpd:'))
+            eq_(True, procs[1].startswith('php-fpm:'))
+            eq_(True, procs[2].startswith('php-fpm-logs:'))
+            eq_(True, procs[3].startswith('web:'))
+        except Exception, e:
+            print str(e)
+            if hasattr(e, 'output'):
+                print e.output
+            if output:
+                print output
+            raise
+        finally:
+            if os.path.exists(bp.bp_dir):
+                shutil.rmtree(bp.bp_dir)
